98dc2b3bd62cfd6960cd5e63beb14d1245d9fa07
besswang <licb8568182@163.com>
2017-01-25 19:39:44 +0800

    layui_12580客服平台

---
+++ static/datas/nav.js
@@ -0,0 +1,31 @@
+var navs = [{
+	"title": "",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "查号转接GS",
+		"icon": "&#xe641;",
+		"href": "../html/one.html"
+	}, {
+		"title": "群发短信",
+		"icon": "&#xe63c;",
+		"href": "../html/two.html"
+	}, {
+		"title": "召开会议",
+		"icon": "&#xe63c;",
+		"href": "../html/three.html"
+	}, {
+		"title": "短信模版",
+		"icon": "&#xe609;",
+		"href": "../html/four.html"
+	}, {
+		"title": "短信历史",
+		"icon": "&#xe62a;",
+		"href": "../html/five.html"
+	}, {
+		"title": "会议",
+		"icon": "&#xe62a;",
+		"href": "../html/meeting.html"
+	}]
+
+}];
